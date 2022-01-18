3b5dedcfe5ac804f92a6f7e95f0dce6b0ed23e8e
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-17 22:03:01 -0500

    Creando estructura inicial de componentes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React, { useState } from "react";
+import data from "./data";
+import Categories from './components/categories.component';
+import Menu from "./components/menu.component";
+
 function App() {
+  const [menuItems, setMenuItems] = useState(data);
+  const [categories, setCategories] = useState([]);
+
   return (
-    <div className="App">
-      <h2>Hello</h2>
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
   );
 }
 
